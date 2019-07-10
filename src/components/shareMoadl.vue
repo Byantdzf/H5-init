aa55bb09a2281ced52e947114451bcc0ac201bb3
DZF <dengzhifeng_63@163.com>
2019-07-10 12:58:18 +0800

    测试

--- src/components/shareMoadl.vue
+++ src/components/shareMoadl.vue
@@ -2,9 +2,9 @@
   <div v-transfer-dom>
     <popup v-model="showModal" :is-transparent="true">
       <div class="bc_share backCover" @click="hideModal">
-        <img src="https://images.ufutx.com/201907/10/c914e9a9edfcd61b3f9af1bb4d022525.png" alt="" width="100%"
-             style="position:fixed;top: 0;z-index: 9999">
         <img src="https://images.ufutx.com/201907/10/c77e6e27f763d29e2a7bf47c4707dcae.png" alt="" width="100%"
+             style="position:fixed;top: 0;z-index: 9999">
+        <img src="https://images.ufutx.com/201907/10/c914e9a9edfcd61b3f9af1bb4d022525.png" alt="" width="100%"
              class="fixed_bot">
       </div>
     </popup>

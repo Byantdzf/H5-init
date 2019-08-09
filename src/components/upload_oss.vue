8a44df7a50ff76368780e339b875d42956849331
DZF <dengzhifeng_63@163.com>
2019-08-09 15:06:51 +0800

    测试

--- src/components/upload_oss.vue
+++ src/components/upload_oss.vue
@@ -20,7 +20,7 @@
   <!--<previewer :list="images" ref="previewer"></previewer>-->
   <!--</div>-->
 <div>
-  <input type="file" @change="onChange" class="weui-uploader__input" mutiple="mutiple" accept='image/*' capture="camera" />
+  <input type="file" @change="onChange" class="weui-uploader__input" mutiple="mutiple" accept='image/png,image/jpeg,image/jpg,image/gif'/>
   <slot name="text"></slot>
 </div>
 

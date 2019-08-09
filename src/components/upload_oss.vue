51f97125b7e409da864ef3e723bc37d656540a32
DZF <dengzhifeng_63@163.com>
2019-08-09 15:51:31 +0800

    测试

--- src/components/upload_oss.vue
+++ src/components/upload_oss.vue
@@ -20,7 +20,7 @@
   <!--<previewer :list="images" ref="previewer"></previewer>-->
   <!--</div>-->
 <div>
-  <input type="file" @change="onChange" class="weui-uploader__input"  accept="image/jpeg,image/jpg,image/png">
+  <input type="file" @change="onChange" class="weui-uploader__input" accept='image/*'/>
   <slot name="text"></slot>
 </div>
 

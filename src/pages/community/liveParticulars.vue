da8548a0d52dd5fb6ada65e6c214be3a9079caf3
web260320689@163.com <web260320689@163.com>
2019-09-25 16:16:38 +0800

    test

--- src/pages/community/liveParticulars.vue
+++ src/pages/community/liveParticulars.vue
@@ -2,7 +2,7 @@
   <div class="z_box" id="orderFullScreen">
     <div class="zone"></div>
     <div class="player-container text-center">
-      <video-player class="vjs-custom-skin" :options="playerOptions" playsinline x5-playsinline x-webkit-airplay='allow'></video-player>
+      <video-player class="vjs-custom-skin" :options="playerOptions" webkit-playsinline x5-playsinline="true" x-webkit-airplay='allow'></video-player>
       <div class="z_person">
         <span class="parent_num">{{click_num}}12</span>
         <img src="https://images.ufutx.com/201909/18/b9db8ba4f8b6134a8df2748c14dbcdf8.png" alt="" class="icon_person">
@@ -499,7 +499,7 @@
         .parent_num{
           width: 66px;
           position: absolute;
-          top: 6px;
+          top: 10px;
           right: 14px;
           font-size: 22px;
           color: white;

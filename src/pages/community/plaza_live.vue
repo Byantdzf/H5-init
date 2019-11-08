ce10ffe2c16e46a956861949631f47cfd9f9f914
zeng <web260320689@163.com>
2019-11-08 15:55:20 +0800

    test

--- src/pages/community/plaza_live.vue
+++ src/pages/community/plaza_live.vue
@@ -20,8 +20,8 @@
                    <!--:src="video_url" :type="video_type" :playOrPause="playOrPause" x-webkit-airplay="allow"-->
                    <!--x5-video-orientation="portrait" @click="pauseVideo" @ended="onPlayerEnded($event)">-->
             <!--</video>-->
-            <video class="video_box" width="100%" height="100%" webkit-playsinline="true" x5-playsinline=""
-                   playsinline preload="auto" x5-video-player-fullscreen=""
+            <video class="video_box" webkit-playsinline="true" x5-playsinline="true"
+                   playsinline preload="auto"
                    :src="video_url" :type="video_type" :playOrPause="playOrPause" x-webkit-airplay="allow"
                    x5-video-orientation="portrait" @click="pauseVideo" @ended="onPlayerEnded($event)">
             </video>

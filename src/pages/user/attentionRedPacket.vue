c3f5bdfaf29ae99fdbe5747334578d223fc67f0e
ZengBin1993 <web260320689@163.com>
2019-12-07 16:31:26 +0800

    test

--- src/pages/user/attentionRedPacket.vue
+++ src/pages/user/attentionRedPacket.vue
@@ -433,7 +433,7 @@
         }
         .z_share_user{
           width: 100%;
-          height: 400px;
+          height: 350px;
           overflow: auto;
           .z_user_photo{
             width: 80px;
